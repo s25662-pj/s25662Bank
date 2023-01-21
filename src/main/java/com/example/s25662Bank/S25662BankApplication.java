f1f067366a9481b68491412691a7c9089ce3fd74
Michał Krokoszyński <s25662@pjwstk.edu.pl>
2023-01-21 12:14:46 +0100

    Initial commit

---
+++ src/main/java/com/example/s25662Bank/S25662BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s25662Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S25662BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S25662BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/s25662Bank/S25662BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s25662Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S25662BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
